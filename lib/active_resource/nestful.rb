882248a21236b93ea133edc726c8bcee2996d5ae
Alex MacCaw <maccman@gmail.com>
2010-10-15 17:45:57 +0100

    have a connection object per class

--- lib/active_resource/nestful.rb
+++ lib/active_resource/nestful.rb
@@ -42,19 +42,16 @@ module ActiveResource
         end
         
         def connection(refresh = false)
-          if defined?(@connection) || superclass == Object
-            @connection = ActiveResource::Nestful::Connection.new(site, format) if refresh || @connection.nil?
-            @connection.proxy = proxy if proxy
-            @connection.user = user if user
-            @connection.password = password if password
-            @connection.auth_type = auth_type if auth_type
-            @connection.timeout = timeout if timeout
-            @connection.ssl_options = ssl_options if ssl_options
-            @connection.oauth = oauth if oauth
-            @connection
-          else
-            superclass.connection
-          end
+          return @connection if @connection
+          @connection = ActiveResource::Nestful::Connection.new(site, format) if refresh || @connection.nil?
+          @connection.proxy = proxy if proxy
+          @connection.user = user if user
+          @connection.password = password if password
+          @connection.auth_type = auth_type if auth_type
+          @connection.timeout = timeout if timeout
+          @connection.ssl_options = ssl_options if ssl_options
+          @connection.oauth = oauth if oauth
+          @connection
         end
       end
       
