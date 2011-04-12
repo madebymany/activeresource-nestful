edc0c44d0264fe8933be4b0c90cc31db185c38f3
Murray Steele <muz@h-lame.com>
2011-04-12 15:25:18 +0100

    Have our nestful integration cope with 422 (ResourceInvalid) errors in a similar fashion to how vanilla-activeresource does.
    e.g. We trap the exception during save and load the errors from the response body into the internal errors object.

--- lib/active_resource/nestful.rb
+++ lib/active_resource/nestful.rb
@@ -5,42 +5,42 @@ require "active_resource"
 
 module ActiveResource
   module Nestful
-    class Base < ActiveResource::Base    
+    class Base < ActiveResource::Base
       class << self
-        
+
         def oauth(options = nil)
           @oauth = options if options
-          
+
           if block_given?
             yield
             @oauth = nil
           end
-          
+
           if defined?(@oauth)
             @oauth
-          elsif superclass != Object && 
-                  superclass != ActiveResource::Base && 
+          elsif superclass != Object &&
+                  superclass != ActiveResource::Base &&
                     superclass.oauth
             superclass.oauth.dup.freeze
           end
         end
-        
+
         alias_method :oauth=, :oauth
-        
+
         def format=(mime_type_reference_or_format)
           format = mime_type_reference_or_format.is_a?(Symbol) ?
             ::Nestful::Formats[mime_type_reference_or_format] : mime_type_reference_or_format
-          
+
           format = format.new
 
           write_inheritable_attribute(:format, format)
           connection.format = format if site
         end
-        
+
         def format
           read_inheritable_attribute(:format) || ::Nestful::Formats::XmlFormat.new
         end
-        
+
         def connection(refresh = false)
           return @connection if @connection
           @connection = ActiveResource::Nestful::Connection.new(site, format) if refresh || @connection.nil?
@@ -54,12 +54,25 @@ module ActiveResource
           @connection
         end
       end
-      
+
       def to_multipart_form(options={})
         self.class.format.encode({
           self.class.element_name => serializable_hash(options)
         })
       end
+
+      def save_with_nestful_validations(options=nil)
+        save_without_nestful_validations(options)
+      rescue ::Nestful::ResourceInvalid => e
+        # cache the remote errors because every call to <tt>valid?</tt> clears
+        # all errors. We must keep a copy to add these back after local
+        # validations
+        @remote_errors = e
+        load_remote_errors(@remote_errors, true)
+        false
+      end
+
+      alias_method_chain :save, :nestful_validations
     end # Base
   end # Nestful
 end # ActiveResource
