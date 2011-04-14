2911fddfe409a65fdd24fbe0751026380300113f
Murray Steele <muz@h-lame.com>
2011-04-14 15:20:27 +0100

    Change the multipart form format when used via activeresource so that it knows about actiondispatch/http/uploadedfile objects and converts them properly.

--- lib/active_resource/nestful/formats/multipart.rb
+++ lib/active_resource/nestful/formats/multipart.rb
@@ -1,9 +1,17 @@
+require 'action_dispatch/http/upload'
+
 module Nestful
   module Formats
     class MultipartFormat < Format
       def extension
         "multipart_form"
       end
+
+      protected
+        def looks_like_a_file_with_actiondispatch_uploads?(value)
+          looks_like_a_file_without_actiondispatch_uploads?(value) || value.is_a?(ActionDispatch::Http::UploadedFile)
+        end
+        alias_method_chain :looks_like_a_file?, :actiondispatch_uploads
     end
   end
 end
